f9dc80c33cca180906c1b46711ddae59d73eb43c
shaileshprajapati1 <shaileshprajapati966@gmail.com>
2023-08-08 16:51:49 +0530

    data

--- Bank_Management_assessment/controller/controller.php
+++ Bank_Management_assessment/controller/controller.php
@@ -44,6 +44,7 @@ class controller  extends model
                     if(isset($_POST['add'])){
                         echo "<pre>";
                         print_r($_POST);
+                        print_r($_FILES);
                         echo "</pre>";
 
                     }
